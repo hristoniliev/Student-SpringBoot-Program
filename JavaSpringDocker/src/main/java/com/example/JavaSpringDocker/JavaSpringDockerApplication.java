ae3d2021caf10ba426349bce18d787b037636674
hristoniliev <85759518+hristoniliev@users.noreply.github.com>
2021-07-05 03:32:03 +0300

    upload

---
+++ JavaSpringDocker/src/main/java/com/example/JavaSpringDocker/JavaSpringDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaSpringDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringDockerApplication.class, args);
+	}
+
+}
---
+++ JavaSpringDocker/src/test/java/com/example/JavaSpringDocker/JavaSpringDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaSpringDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
